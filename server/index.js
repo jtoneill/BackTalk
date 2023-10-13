54ceb6bcdc6cc5a074331706db8354b86c8c9af4
jtoneill <tyleroneill21@gmail.com>
2023-10-13 14:51:45 -0700

    Removed the router

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const path = require('path');
-const router = require('./routes');
+// const router = require('./routes');
 
 const app = express();
 
@@ -12,7 +12,7 @@ app.use(morgan('dev'));
 // app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.use('/', router);
+// app.use('/', router);
 
 app.listen(process.env.SV_PORT);
 console.log(`Listening at http://localhost:${process.env.SV_PORT}`);
